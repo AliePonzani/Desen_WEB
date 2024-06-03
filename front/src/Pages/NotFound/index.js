9244c1bd1414f10805759076ed1b93a9da2f0123
Guilherme Picasso <guipicasso3@gmail.com>
2024-06-03 07:39:31 -0300

    notfound

--- front/src/Pages/NotFound/index.js
+++ front/src/Pages/NotFound/index.js
@@ -1,12 +1,12 @@
-import 'index.scss';
+import './index.scss';
 
-export default function notFound() {
+export default function NotFound() {
 
     return (
         <div className="notFound_page">
             <div className="mensagem">
                 <h1>404</h1>
-                <h3>the page you seek does not exist</h3>
+                <h3>Essa página não existe</h3>
             </div>
         </div>
 
--- front/src/index.js
+++ front/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/index.js';
 import Cardapio from './Pages/Cardapio/index.js';
 import Painel from './Pages/Painel/index.js';
-import notFound from './Pages/notFound/index.js';
+import NotFound from './Pages/NotFound/index.js';
 
 const user = "teste 1";
 const senha = "12345";
@@ -20,7 +20,7 @@ root.render(
         <Route path='/' element={<Home />} />
         <Route path={`/painel/${user}/${senha}`} element={<Painel/>}/>
         <Route path='/cardapio/:cardapio' element={<Cardapio />} />
-        <Route path='*' element={<notFound />} />
+        <Route path='*' element={<NotFound />} />
         
       </Routes>
     </BrowserRouter>
